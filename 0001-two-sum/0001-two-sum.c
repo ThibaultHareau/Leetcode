0ba7ecd19f6aaa975dc6ac645e9156633136cb21
Thibault Hareau <hareaut@gmail.com>
2025-03-16 04:36:09 +0100

    Time: 116 ms (9.17%), Space: 9 MB (13.26%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -4,14 +4,39 @@
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     int* result = (int *)malloc(sizeof(int) * 2);
     *returnSize = 2;
-    for (int i=0;i<numsSize-1;i++){
-        for(int j=i+1;j<numsSize;j++){
-            if (nums[i]+nums[j] == target){
+    typedef struct node
+    {
+        int value;
+        int position;
+        struct node* next;
+    }
+    node;
+    node* head = NULL;
+    head = (node *) malloc(sizeof(node));
+    head->value = nums[0];
+    head->position = 0;
+    head->next = NULL;
+    node* current;
+    for (int i=1;i<numsSize;i++){
+        current = head;
+        while(current != NULL){
+            if (nums[i] + current->value == target){
                 result[0] = i;
-                result[1] = j;
+                result[1] = current->position;
                 return result;
             }
+            if (nums[i] == current->value){
+                break;
+            }
+            if (current->next == NULL){
+                current->next = (node *) malloc(sizeof(node));
+                current->next->value = nums[i];
+                current->next->position = i;
+                current->next->next = NULL;
+                break;
+            }
+            current = current->next;
         }
     }
-    return returnSize;
+    return result;
 }
