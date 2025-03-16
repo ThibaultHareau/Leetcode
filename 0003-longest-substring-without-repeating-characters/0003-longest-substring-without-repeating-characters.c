579a7538a3733ffe00eb5731cc363b70f66ae9cc
Thibault Hareau <hareaut@gmail.com>
2025-03-16 07:02:23 +0100

    Time: 162 ms (10.22%), Space: 9.3 MB (74.14%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -0,0 +1,35 @@
+int lengthOfLongestSubstring(char* s) {
+    int *ascii = (int *)calloc(sizeof(int), 128);
+    for (int i=0;i<128;i++) {ascii[i]=-1;}
+    if (strlen(s) == 0) return 0;
+    int maxlen = 1;
+    bool exited;
+    for (int i=0;i<strlen(s)-1;i++){
+        printf("%c %d => %d\n", s[i], ascii[s[i]], maxlen);
+        if (maxlen + i > strlen(s)) return maxlen;
+        ascii[s[i]] = i;
+        exited = false;
+        for (int j=i+1;j<strlen(s);j++){
+            if (ascii[s[j]] < j && ascii[s[j]] > -1){
+                exited = true;
+                if (maxlen < j-i){
+                    maxlen = j-i;
+                }
+                break;
+            }
+            ascii[s[j]] = j%128;
+            if (s[j] == 121){
+                printf("------ALLOC %c ===> %d   %d\n", s[j], ascii[s[j]], j);
+            }
+        }
+        if (!exited){
+            return strlen(s) - i ;
+        }
+        ascii[s[i]] = -1;
+        if (s[i] == 121){
+            printf("------ALLOC %c ===> %d\n", s[i], ascii[s[i]]);
+        }
+    }
+    free(ascii);
+    return maxlen;
+}
