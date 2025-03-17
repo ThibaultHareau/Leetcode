21830ab82d1f17806b4a384e8f9a7591f4291709
Thibault Hareau <hareaut@gmail.com>
2025-03-17 02:44:18 +0100

    Time: 4 ms (28.44%), Space: 8.3 MB (30.08%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.c
+++ 0009-palindrome-number/0009-palindrome-number.c
@@ -1,17 +1,15 @@
 bool isPalindrome(int x) {
     if (x < 0) return false;
     if (x < 10) return true;
-    int numlen;
-    if (x < 2147483647) {
+    double numlen;
+    if (x != 2147483647) {
         numlen = (int)(ceil(log10(x+1)));
     }
     else {
         numlen = (int)(ceil(log10(x)));
     }
-    char str[numlen + 1];
-    sprintf(str, "%d", x);
-    for (int i=0;i<(numlen/2);i++){
-        if (str[i] != str[numlen-i-1]) return false;
+    for (double i=0;i<(numlen/2);i++){
+        if ((x/((int) pow(10.,i)))%10 != (x/((int) pow(10.,numlen-i-1)))%10) return false;
     }
     return true;
 }
