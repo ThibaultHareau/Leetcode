f2387afbd7bf7563e7634d98aed01f421abf7149
Thibault Hareau <hareaut@gmail.com>
2025-03-25 00:11:04 +0100

    Time: 198 ms (93.96%), Space: 17.9 MB (48.68%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,6 +3,8 @@ class Solution:
         nums.sort()
         result = -10**6
         for i in range(len(nums)-2):
+            if nums[i]*3 > target + abs(target-result):
+                return result
             j = i+1
             k = len(nums)-1
             while j!=k:
