b919c1b1e5c2ee40886946dfc27272b7cd84e252
Thibault Hareau <hareaut@gmail.com>
2025-03-30 00:36:34 +0100

    Time: 0 ms (100%), Space: 7.8 MB (62.93%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.c
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -2,12 +2,13 @@ int climbStairs(int n) {
     if (n == 1) return 1;
     if (n == 2) return 2;
     int i=2;
-    int previous[2] = {1, 2};
+    int prev2 = 1;
+    int prev1 = 2;
     int ways;
     while (i<n) {
-        ways = previous[0]+previous[1];
-        previous[0] = previous[1];
-        previous[1] = ways;
+        ways = prev2+prev1;
+        prev2 = prev1;
+        prev1 = ways;
         i++;
     }
     return ways;
